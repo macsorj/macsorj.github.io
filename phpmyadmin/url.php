6bb94e23013689014e970e31733a34b8cf55a74f
Macsorj <verento@sbcglobal.net>
2020-06-05 17:09:10 -0500

    Inital Site Generation

---
+++ phpmyadmin/url.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://mc.fight4life.tk/phpmyadmin/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://mc.fight4life.tk/phpmyadmin/";
+		</script>
+
+		<p>You are being redirected to <a href="http://mc.fight4life.tk/phpmyadmin/">http://mc.fight4life.tk/phpmyadmin/</a></p>
+	</body>
+</html>
---
+++ standalone/config.js
@@ -0,0 +1,11 @@
+var config = {
+ url : {
+  configuration: 'up/configuration',
+  update: 'up/world/{world}/{timestamp}',
+  sendmessage: 'up/sendmessage',
+  login: 'up/login',
+  register: 'up/register',
+  tiles: 'tiles/',
+  markers: 'tiles/'
+ }
+};
